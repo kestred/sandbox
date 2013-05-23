357085efa6c8fa85b43d28de68dca3e7edd784e9
Kestred <kestred@riotcave.com>
2013-05-23 08:31:53 -0600

    Cleaned client/chat.js formatting.

--- src/client-core/js/chat.js
+++ src/client-core/js/chat.js
@@ -6,16 +6,16 @@ mods['chat'] = new Argonaut.Module('chat');
     chat.run = function() {
         argo.loader.update('Connecting to Chat');
         var socket = io.connect(document.URL + 'chat');
-		argo.sockets.chat = chat.socket = socket;
+        argo.sockets.chat = chat.socket = socket;
         socket.on('chat', function(data) {
             var name = 'SYSTEM';
-			if(data.playerId == argo.localPlayer.id) {
-				name = argo.localPlayer.name;
-			} else if(data.playerId == argo.gamemaster.id) {
-				name = argo.gamemaster.name;
-			} else if(data.playerId in argo.players) {
-				name = argo.players[data.playerId].name;
-			}
+            if(data.playerId == argo.localPlayer.id) {
+                name = argo.localPlayer.name;
+            } else if(data.playerId == argo.gamemaster.id) {
+                name = argo.gamemaster.name;
+            } else if(data.playerId in argo.players) {
+                name = argo.players[data.playerId].name;
+            }
             chat.chatHistory.logMessage(data.message, name);
         });
 
