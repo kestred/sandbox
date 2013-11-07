d79a2a949f27b063580ff88d2bad6219beb36aa5
kestred <kestred@riotcave.com>
2013-11-07 03:12:56 -0700

    Update install script.

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 mkdir -p /usr/local/lib/easyirc
-cp src/easyirc.py /usr/local/lib/easyirc/
+cp -r src/* /usr/local/lib/easyirc/
 ln -s /usr/local/lib/easyirc/easyirc.py /usr/local/bin/easyirc
 
-cp man/easyirc.1 /usr/share/man/
+cp man/easyirc.1 /usr/share/man/man1
+gzip /usr/share/man/man1/easyirc.1
