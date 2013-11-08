d23dcab0c412d94fdfa1e8d447131ab693a7f648
kestred <kestred@riotcave.com>
2013-11-08 01:36:48 -0700

    Refactor: Update and improve install script

--- install.sh
+++ install.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
-mkdir -p /usr/local/lib/easyirc
-cp -r src/* /usr/local/lib/easyirc/
-ln -s /usr/local/lib/easyirc/easyirc.py /usr/local/bin/easyirc
+# Add the libraries to /usr/local/lib
+mkdir -p /usr/local/lib/potion
+cp -r src/* /usr/local/lib/potion/
 
-cp man/easyirc.1 /usr/share/man/man1
-gzip /usr/share/man/man1/easyirc.1
+# Add the tool to /usr/local/bin
+ln -s /usr/local/lib/potion/irc-potion.py /usr/local/bin/potion
+
+# Create work directory
+mkdir -p /tmp/potion/man1
+
+# Prepare the man files
+cp man/*.1 /tmp/potion/man1
+gzip /tmp/potion/man1/*.1
+
+# Add the man files to /usr/share/man
+cp /tmp/potion/man1/*.1 /usr/share/man/man1
+
+# Cleanup
+rm -r /tmp/potion
