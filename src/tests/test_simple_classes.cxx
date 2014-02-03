4d696e6641ac2f35620f1037e2ef8f933cc645dc
kestred <kestred@riotcave.com>
2014-02-03 07:14:16 -0700

    Update class tests to no longer ask for symbols, we don't use it any more.

--- src/tests/test_simple_classes.cxx
+++ src/tests/test_simple_classes.cxx
@@ -25,18 +25,6 @@ UNITTEST(test_simple_classes) {
 	assert(module != NULL, "Encountered errors while parsing test_simple_classes.src");
 
 	Scope* global = &module->files.find(filename)->second->scope;
-	// Check our structs have corresponding symbols
-	assert(global->symbols.find("A") != global->symbols.end(), "Symbol 'struct A' doesn't exist.");
-	assert(global->symbols.find("B") != global->symbols.end(), "Symbol 'struct B' doesn't exist.");
-
-	// Check our classes have corresponding symbols
-	assert(global->symbols.find("C") != global->symbols.end(), "Symbol 'class C' doesn't exist.");
-	assert(global->symbols.find("D") != global->symbols.end(), "Symbol 'class D' doesn't exist.");
-	assert(global->symbols.find("E") != global->symbols.end(), "Symbol 'class E' doesn't exist.");
-
-	// Check no extra bogus symbols exist
-	assert_equal(global->symbols.size(), 5);
-
 	// Check we have a type defined for each struct/class
 	assert(global->types.find("A") != global->types.end(), "Type 'A' doesn't exist.");
 	assert(global->types.find("B") != global->types.end(), "Type 'B' doesn't exist.");
